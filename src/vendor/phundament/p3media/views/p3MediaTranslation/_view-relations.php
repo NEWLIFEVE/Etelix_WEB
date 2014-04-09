d4849ba684963efaccd3dad6eae0cd84e1776e89
Manuel Zambrano <mmzmm3z@gmail.com>
2014-04-09 11:30:49 -0430

    eliminando carpeta vendors

--- src/vendor/phundament/p3media/views/p3MediaTranslation/_view-relations.php
+++
@@ -1,6 +0,0 @@
-
-<!--
-<h2>
-    <?php echo Yii::t('P3MediaModule.crud', 'Relations') ?></h2>
--->
-
--- src/vendor/phundament/p3widgets/components/views/widgetDisplay.php
+++
@@ -1,3 +0,0 @@
-<div class="widget display" id="<?php echo P3WidgetContainer::WIDGET_CSS_PREFIX . $model->id ?>">
-    <?php echo $content; ?>
-</div>
