e18ff3eb1ee1abc419fdf31596676214b4bcebf5
ramon2do <pnfiuty.rramirez@gmail.com>
2014-04-14 11:20:39 -0430

    Correcciones en Responsive

--- src/protected/themes/frontend/views/site/index.php
+++ src/protected/themes/frontend/views/site/index.php
@@ -1,5 +1,5 @@
-<div class="row">
-	<div class="span12">
+<div class="row-fluid">
+	<div class="">
 		<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main')) ?>
 	</div>
 </div>
