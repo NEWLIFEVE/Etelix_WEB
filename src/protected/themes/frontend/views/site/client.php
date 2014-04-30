5a708304b2640b6dfa753ef986da9e4f13208da7
Charlotte Hernandez <luzneffer@gmail.com>
2014-04-30 18:53:03 -0430

    vista de clientes e imagenes de clientes

---
+++ src/protected/themes/frontend/views/site/client.php
@@ -0,0 +1,12 @@
+
+<div class="row-fluid">
+	<div class="">
+		<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main')) ?>
+	</div>
+</div>
+<br/>
+<div style="margin-bottom:18px;" align="center">
+<img src="/images/cliente_imagen.png">
+</div><br/>
+
+
