d18a99e8532aa72249dfa5197646873f1f997d1a
Manuel Zambrano <mmzmm3z@gmail.com>
2014-07-03 11:45:33 -0430

    cambios

--- src/protected/config/main-local.php
+++ src/protected/config/main-local.php
@@ -1,22 +1,18 @@
 <?php
-
 // Use this file as main-local.php to override settings only on your local machine
-
-return array(
-    'params' => array(
-        'env' => 'development'
-    ),
-    'components' => array(
-        // MySQL
-
-//        'db'            => array(
-//            'tablePrefix'      => '',
-//            'connectionString' => 'mysql:host=172.16.17.190;dbname=etel7527_web_phundament',
-//            'emulatePrepare' => true,
-//            'username' => 'manuelz',
-//            'password' => '123',
-//            'charset' => 'utf8',
-//        ),
-
-    )
+return array('params' =>
+array('env' => 'development'),
+'components' =>
+array(
+	// MySQL
+	'db' =>
+	array(
+		'tablePrefix' => '',
+		'connectionString' => 'mysql:host=172.16.17.190;dbname=etelix_web',
+		'emulatePrepare' => true,
+		'username' => 'manuelz',
+		'password' => '123',
+		'charset' => 'utf8',
+	),
+)
 );
--- src/web/index.php
+++ src/web/index.php
@@ -1,46 +1,29 @@
 <?php
-date_default_timezone_set('America/Caracas');
 //Definimos nuestro servidor de produccion
 define('SERVER_NAME_PROD', 'www.etelix.com');
-//Definimos nuestro servidor de preproduccion
-define('SERVER_NAME_PRE_PROD', 'new.etelix.com');
-//Definimos nuestro servidor de desarrollo
-define('SERVER_NAME_DEV', 'etelix.web.local');
 //Obtenemos el nombre del servidor actual
-$server=$_SERVER['SERVER_NAME'];
+$server = $_SERVER['SERVER_NAME'];
 // config files
 $dir = realpath(dirname(__FILE__).'/..').'/';
-$main   = require('../protected/config/main.php');
-$local  = require('../protected/config/main-local.php');
-$env    = require('../protected/config/env-'.$local['params']['env'].'.php');
-
+$main = require ('../protected/config/main.php');
+$local = require ('../protected/config/main-local.php');
+$env = require ('../protected/config/env-'.$local['params']['env'].'.php');
 // define YII_DEBUG in config files
-switch ($server)
-{
+switch ($server) {
 	case SERVER_NAME_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',false);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+		defined('YII_DEBUG') or define('YII_DEBUG', false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 0);
 		break;
-	case SERVER_NAME_PRE_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-		break;
-	case SERVER_NAME_DEV:
 	default:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		defined('YII_DEBUG') or define('YII_DEBUG', true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 		break;
 }
-
 // register composer autoloader
-require_once('../vendor/autoload.php');
-
+require_once ('../vendor/autoload.php');
 // load Yii
-require_once('../vendor/yiisoft/yii/framework/yii.php');
-
+require_once ('../vendor/yiisoft/yii/framework/yii.php');
 // merge configurations
-$config = CMap::mergeArray($main,$env,$local);
-
+$config = CMap::mergeArray($main, $env, $local);
 // start web application
 Yii::createWebApplication($config)->run();
-
